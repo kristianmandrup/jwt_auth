95df194143aeca880ce0f7c0b43d8c3ce3777390
sandy_rocks <sandy.singh21031988@gmail.com>
2015-02-23 15:46:00 -0500

    Added two utility classes for JwtAuth.

--- lib/JwtAuth.rb
+++ lib/JwtAuth.rb
@@ -1,9 +1,54 @@
 require "JwtAuth/version"
 module JwtAuth 
-  def self.say
-    "Hello geeks!"
+  class TokenValidator
+    def initialize(token)
+      @token = token
+    end
+
+    def valid_jwt_token?
+       user_identifier == token_identifier if decoded_data.present?
+    end
+
+    def user_identifier
+      user.email
+    end
+
+    def token_identifier
+       decoded_data.first[user_id.to_s]
+    end
+
+    def user
+       @user ||= User.find_by_id(user_id)
+    end
+
+    def user_id
+      @user_id ||= decoded_data.first.keys.first.to_i
+    end
+
+    def decoded_data
+       JWT.decode(@token, nil, false) rescue ""
+    end
   end
-  def self.greet
-    "Congratulations! Your rubygem is working."
+
+  class UserAuthValidator
+    def initialize(auth_params)
+      auth_params = auth_params
+    end
+
+    def get_token
+      get_token_params if user
+    end
+
+    def get_token_params
+      { "token" => generate_valid_tokens(@user), "username" => @user.email }
+    end
+
+    def user
+      @user = User.find_or_create_by(auth_params)
+    end
+
+    def generate_valid_tokens(user)
+      JWT.encode({@user.id.to_s => @user.email.to_s}, "secret")
+    end
   end
 end
